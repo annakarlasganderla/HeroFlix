053af3dec61f88318f843266e93df974de562e74
anna sganderla <sganderlakarlaanna@gmail.com>
2021-10-11 20:08:40 -0300

    adicionando carrossel funcional

--- src/components/MovieRow/MovieRow.js
+++ src/components/MovieRow/MovieRow.js
@@ -6,11 +6,22 @@ export default ({title, items}) => {
     const [scrollX, setScrollX] = useState(0);
 
     const handleLeftArrow = () => {
-
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
     }
 
     const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 150; // largura da lista inteira
 
+        if ((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        
+        setScrollX(x);
     }
 
 
